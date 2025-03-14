ad1b773ab8028069841ed59aac3d3f63afc0de39
“Richie <3068009m@student.gla.ac.uk>
2025-03-14 13:38:43 +0000

    Chapter 8 with all tests passing

--- rango/views.py
+++ rango/views.py
@@ -20,8 +20,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-    return render(request, 'rango/about.html')
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -41,13 +42,12 @@ def show_category(request, category_name_slug):
 def add_category(request):
     form = CategoryForm()
 
-
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -60,7 +60,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
