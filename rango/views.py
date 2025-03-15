12d4c87728dd3e3c955a6a809f6f35a6805c0501
“Richie <3068009m@student.gla.ac.uk>
2025-03-15 14:50:57 +0000

    chapter 9 all tests passing

--- rango/views.py
+++ rango/views.py
@@ -45,6 +45,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -59,6 +60,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -127,7 +129,6 @@ def user_login(request):
         user = authenticate(username=username, password=password)
 
         if user:
-
             if user.is_active:
                 login(request, user)
                 return redirect(reverse('rango:index'))
@@ -142,7 +143,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
